bc950c14d722c4ac6b5560e87c94eab16727acf1
Jake Berg <jacobvictorberg@gmail.com>
2018-02-08 14:22:10 -0500

    finished assignment

--- anagrams.js
+++ anagrams.js
@@ -1,14 +1,23 @@
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
+
 document.getElementById("findButton").onclick = function () {
 
     var typedText = document.getElementById("input").value;
 
+    var dest = document.getElementById("main");
 
-    function alphabetize(a) {
-        return a.toLowerCase().split("").sort().join("").trim();
+    for (i = 0; i < words.length; i++) {
+        word = words[i];
+        if (alphabetize(typedText) == alphabetize(word)) {
+            var newSpan = document.createElement("span");
+            var text = document.createTextNode(word + " ");
+            newSpan.appendChild(text);
+            dest.appendChild(newSpan);
+            
+        }
     }
 
-    alphabetize(typedText)
-
-    
 
 }
