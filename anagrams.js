d2ecb0b15bb1b6cbfc8e3d0233fafdc729e44589
Jake Berg <jacobvictorberg@gmail.com>
2018-03-21 10:08:44 -0400

    assignment complete

--- anagrams.js
+++ anagrams.js
@@ -1,23 +1,37 @@
+const dest = document.getElementById("main");
+
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
 
-document.getElementById("findButton").onclick = function () {
 
-    var typedText = document.getElementById("input").value;
 
-    var dest = document.getElementById("main");
+function findAll() {
+    var countAnagrams = {}
 
     for (i = 0; i < words.length; i++) {
-        word = words[i];
-        if (alphabetize(typedText) == alphabetize(word)) {
-            var newSpan = document.createElement("span");
-            var text = document.createTextNode(word + " ");
-            newSpan.appendChild(text);
-            dest.appendChild(newSpan);
-            
+        var word = words[i];
+        const value = alphabetize(word);
+
+            if (value in countAnagrams) {
+                countAnagrams[value].push(word);
+            } else {
+                countAnagrams[value] = [word];
+            }
+                
+            if (countAnagrams[value].length > 4) {
+                let newSpan = document.createElement("span");
+                let text = document.createTextNode(countAnagrams[value] + ":::::::");
+                newSpan.appendChild(text);
+                dest.appendChild(newSpan); 
+            }
         }
     }
+   
 
 
-}
+
+
+
+
+document.getElementById("findButton").addEventListener("click", findAll)
